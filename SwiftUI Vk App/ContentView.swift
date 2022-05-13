ee41250ef027e6e1c24cb3b106d4979ca0262a04
Anton H <anton.g@ksyste.ms>
2022-05-13 23:38:56 +0200

    first comit

---
+++ SwiftUI Vk App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Vk App
+//
+//  Created by Anton Hodyna on 13/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Vk App/SwiftUI_Vk_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Vk_AppApp.swift
+//  SwiftUI Vk App
+//
+//  Created by Anton Hodyna on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Vk_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
